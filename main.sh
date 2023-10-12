4896a9b525214e0eea107a95ee9543520dfac6ff
l-isaro <l.isaro@alustudent.com>
2023-10-12 03:56:08 -0700

    task 1

--- main.sh
+++ main.sh
@@ -1,14 +1,32 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 while true
-      read -p "Enter the option for ewhat task you want to perform(enter for entering records, view for viewing records, delete for deleting record/
-      update for updating records and exit for exiting the application)" option
-      if (option == "enter")
-	 read -p "enter your name, email, age, STUDENTID" name email age STUDENTID
-	 echo "$name $email $age $STUDENTID" >> students-list_0923.txt
-      elif (option == "view")
+do
+    read -r -p "Enter the option for ewhat task you want to perform(enter for entering records, view for viewing records, delete for deleting record update for updating records and exit for exiting the application)" option
+    echo "$option"
+      if [[ $option == "enter" ]]
+      then
+	   read -p "enter your name, email, age, STUDENTID" name email age STUDENTID
+	   echo "$name $email $age $STUDENTID" >> students-list_0923.txt
+	   echo "Record added successfully"
+      elif [[ $option == "view" ]]
+      then
 	   cat students-list_0923.txt
-      elif (option == "delete")
+      elif [[ $option == "delete" ]]
+      then
 	   read -p "enter the STUDENTID" temp_id
-	   sed -i "$temp_id" students-list_0923.txt
-      elif (option == "")
+	   sed -i "/$temp_id/d" students-list_0923.txt
+	   echo "Record deleted successfully"
+      elif [[ $option == "update" ]]
+      then
+	   read -p "enter the STUDENTID of the student who's information you want to update" update_id
+	   read -p "enter the updated name, email and age " updated_name updated_email updated_age
+	   sed -i "s/^.*,$update_id$/$updated_name,$updated_email,$updated_age$update_id/" students-list_0923.txt
+	   echo "Record updated successfully"
+      elif [[ $option == "exit" ]]
+      then
+	   exit 0
+      else
+	  echo "Invalid input"
+      fi
+done	   
 
