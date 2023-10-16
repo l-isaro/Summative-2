8f26cec29fa941977fea49487fa07a515b271d22
lemwaizz <mwaidennis26@gmail.com>
2023-10-16 13:13:49 -0700

    move-to-directory.sh

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,2 +1,3 @@
 #/usr/bin/env bash
-mv students-list_0923.txt main.sh select-emails.sh student-emails.txt negpod_14-q1
+mkdir negpod_id-q1
+mv -t negpod_id-q1 ~/Summative-2/main.sh ~/Summative-2/Students-list_0923.txt ~/Summative-2/Select-emails.sh ~/Summative-2/student-emails.txt
