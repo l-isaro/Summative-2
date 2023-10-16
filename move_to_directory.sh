69785fbc063e7f0f0f8f63edb4be3e363dfaaf99
lemwaizz <mwaidennis26@gmail.com>
2023-10-16 12:04:31 -0700

    creation included

--- move_to_directory.sh
+++ move_to_directory.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 #move files  to specified directory 
+mkdir negpod_id-q1
 mv -t negpod_id-q1 ~/Summative-2/main.sh ~/Summative-2/Students-list_0923.txt ~/Summative-2/Select-emails.sh ~/Summative-2/student-emails.txt
